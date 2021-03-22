8d9dabb5a2ef8c9d99fd54051e5dc37ba594d44e
fholstege <31654652+fholstege@users.noreply.github.com>
2021-03-22 23:34:07 +0100

    synthetic oversampling, logged

--- compare_models.py
+++ compare_models.py
@@ -14,6 +14,7 @@ from sklearn import preprocessing
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import smogn
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_log_error
 
 # import custom models defined in define_models
 from define_models import get_base_model, get_base_model_with_dropout_batchNorm
@@ -127,7 +128,7 @@ history_extended = base_model_extended.fit(X_train, y_train, validation_data=(X_
 # test set is here our own defined one
 base_model_extended.load_weights(filepath_models + 'base_model_extended_weights.hdf5')
 
-print('Training Loss : {}\nValidation Loss : {} \n Test Loss: {}'.format(base_model_extended.evaluate(X_train, y_train), base_model_extended.evaluate(X_valid, y_valid), base_model_extended.evaluate(X_test, y_test)))
+print('Results extended model - \n Training Loss : {}\nValidation Loss : {} \n Test Loss: {}'.format(base_model_extended.evaluate(X_train, y_train), base_model_extended.evaluate(X_valid, y_valid), base_model_extended.evaluate(X_test, y_test)))
 
 #####################
 # In this part of the code, we add synthetic data to oversample from rare classes from
@@ -141,14 +142,60 @@ checkpoints_synthetic = ModelCheckpoint(
           save_weights_only=True,
           verbose=1)
 
+# base model with synthetic
+base_model_synthetic = get_base_model(input_dim=24, base_n_nodes=24, multiplier_n_nodes = 0.5)
+base_model_synthetic.summary()
+
 
 # train the model, optimizing with validation
-history_synthetic = base_model.fit(X_train_synthetic, y_train_synthetic, validation_data=(X_valid, y_valid),
+history_synthetic = base_model_synthetic.fit(X_train_synthetic, y_train_synthetic, validation_data=(X_valid, y_valid),
           epochs=100, batch_size=1, callbacks=[early_stop ,checkpoints_synthetic])
 
 
 # load the best weights from the model, then check how performs in all sets
 # test set is here our own defined one
-base_model.load_weights(filepath_models + 'base_model_synthetic_weights.hdf5')
+base_model_synthetic.load_weights(filepath_models + 'base_model_synthetic_weights.hdf5')
 
-print('base model with batch and dropout, and with synthetic data - \n Training Loss : {}\nValidation Loss : {} \n Test Loss: {}'.format(base_model_extended.evaluate(X_train, y_train), base_model_extended.evaluate(X_valid, y_valid), base_model_extended.evaluate(X_test, y_test)))
+print('Base model  with synthetic data - \n Training Loss : {}\nValidation Loss : {} \n Test Loss: {}'.format(base_model_extended.evaluate(X_train, y_train), base_model_extended.evaluate(X_valid, y_valid), base_model_extended.evaluate(X_test, y_test)))
+
+
+#####################
+# In this part of the code, we log the dependent - subsequently, we revert it back, and test the prediction accuracy
+#
+#####################
+
+# save the weights of the model here
+checkpoints_logged = ModelCheckpoint(
+          filepath_models + 'base_model_logged_weights.hdf5', 
+          save_best_only=True, 
+          save_weights_only=True,
+          verbose=1)
+
+# base model with logged data
+base_model_logged = get_base_model(input_dim=24, base_n_nodes=24, multiplier_n_nodes = 0.5)
+base_model_logged.summary()
+
+
+# train the model, optimizing with validation
+history_logged = base_model_logged.fit(X_train, np.log(1 + y_train), validation_data=(X_valid, np.log(1 + y_valid)),
+          epochs=100, batch_size=1, callbacks=[early_stop ,checkpoints_logged])
+
+
+# load the best weights from the model, then check how performs in all sets
+# test set is here our own defined one
+base_model_logged.load_weights(filepath_models + 'base_model_logged_weights.hdf5')
+
+# get logged predictions, backtransform
+logged_predictions_train = base_model_logged.predict(X_train)
+logged_predictions_valid = base_model_logged.predict(X_valid)
+logged_predictions_test = base_model_logged.predict(X_test)
+
+backtransformed_predictions_train = np.exp(logged_predictions_train) - 1
+backtransformed_predictions_valid = np.exp(logged_predictions_valid) - 1
+backtransformed_predictions_test = np.exp(logged_predictions_test) - 1
+
+msle_train = mean_squared_log_error(y_train, backtransformed_predictions_train)
+msle_valid = mean_squared_log_error(y_valid, backtransformed_predictions_valid)
+msle_test = mean_squared_log_error(y_test, backtransformed_predictions_test)
+
+print('Base model  with logged data - \n Training Loss : {}\nValidation Loss : {} \n Test Loss: {}'.format(msle_train, msle_valid, msle_test))
